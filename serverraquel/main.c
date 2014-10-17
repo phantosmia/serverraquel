14c3d28d0729813739feccc606198f89358952b2
Raquel Natali Mariz <raquelnatalimariz@raquels-air.csusm.edu>
2014-10-17 15:13:31 -0700

    Initial Commit

---
+++ serverraquel/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  serverraquel
+//
+//  Created by Raquel Natali Mariz on 10/17/14.
+//  Copyright (c) 2014 Raquel Natali Mariz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
